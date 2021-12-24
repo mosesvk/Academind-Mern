e2a9a9e940ab2e5c9cf481d95112658517b98874
mosesvk <kaumatule93@gmail.com>
2021-12-23 18:40:43 -0700

    update

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Users from "./user/pages/Users";
 
 const App = () => {
-  return <h1>Let's start!</h1>;
-}
+  return (
+    <Router>
+      <Route path='/' exact component={Users}/>
+      <Redirect to='/' />
+    </Router>
+  );
+};
 
 export default App;
