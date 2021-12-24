969cdd6479b518dbe78b15e60e84d27fc7aad642
mosesvk <kaumatule93@gmail.com>
2021-12-24 09:58:22 -0700

    update

--- 01-starting-setup/src/shared/components/UIElements/MainNavigation.js
+++ 01-starting-setup/src/shared/components/UIElements/MainNavigation.js
@@ -6,7 +6,17 @@ import './MainNavigation.css'
 const MainNavigation = () => {
   return (
     <MainHeader>
-      
+      <button className='main-navigation__menu-btn'>
+        <span />
+        <span />
+        <span />
+      </button>
+      <h1 className='main-navigation__title'>
+        <Link to='/'>New Places</Link>
+      </h1>
+      <nav>
+        ...
+      </nav>
     </MainHeader>
   )
 }
