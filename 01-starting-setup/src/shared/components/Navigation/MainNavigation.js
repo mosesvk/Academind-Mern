dce144eccb7ab85c220612fc60ebc9185eacfe9a
mosesvk <kaumatule93@gmail.com>
2021-12-24 16:33:35 -0700

    update

--- 01-starting-setup/src/shared/components/Navigation/MainNavigation.js
+++ 01-starting-setup/src/shared/components/Navigation/MainNavigation.js
@@ -22,13 +22,11 @@ const MainNavigation = () => {
   return (
     <>
       {drawerIsOpen && <Backdrop closeDrawerHandler={closeDrawerHandler} />}
-      {drawerIsOpen && (
-        <SideDrawer>
-          <nav className="main-navigation__drawer-nav">
-            <NavLinks />
-          </nav> 
-        </SideDrawer>
-      )}
+      <SideDrawer show={drawerIsOpen}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav> 
+      </SideDrawer>
       <MainHeader>
         <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
           <span />
--- 01-starting-setup/src/shared/components/Navigation/SideDrawer.js
+++ 01-starting-setup/src/shared/components/Navigation/SideDrawer.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
-import './SideDrawer.css'
+import "./SideDrawer.css";
 
 const SideDrawer = (props) => {
   const content = (
-    <aside className='side-drawer'>
-      {props.children}
-    </aside>)
-
-
-  return (
-    ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer">{props.children}</aside>
+    </CSSTransition>
   );
-}
 
-export default SideDrawer
+  return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
+};
+
+export default SideDrawer;
