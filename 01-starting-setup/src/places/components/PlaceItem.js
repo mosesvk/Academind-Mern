ddb42bbc0b4a02a59701addc719ee61ba1b66ee7
mosesvk <kaumatule93@gmail.com>
2021-12-24 17:26:38 -0700

    update

--- 01-starting-setup/src/places/components/PlaceItem.js
+++ 01-starting-setup/src/places/components/PlaceItem.js
@@ -2,11 +2,19 @@ import React from 'react'
 
 import './PlaceItem.css'
 
-const PlaceItem = () => {
+const PlaceItem = (props) => {
+
   return (
-    <div>
-      
-    </div>
+    <li className='place-item'>
+      <div className='place-item__image'>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className='place-item__info'>
+        <h2>{props.title}</h2>
+        <h3>{props.address}</h3>
+        <p>{props.description}</p>
+      </div>
+    </li>
   )
 }
 
