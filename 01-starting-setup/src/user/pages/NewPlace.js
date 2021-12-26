a4d3dfe8f30d43ee5111c40cf7c99a4f45062a0f
mosesvk <kaumatule93@gmail.com>
2021-12-26 13:55:56 -0700

    update

---
+++ 01-starting-setup/src/user/pages/NewPlace.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const NewPlace = () => {
+  return (
+    <form className='place-form'>
+      <Input type='text' label='Title' validators={[]} onChange={} />
+    </form>
+  )
+}
+
+export default NewPlace
