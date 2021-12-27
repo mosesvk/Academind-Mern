2ab44e387555ec0105426a124d24d366a4374279
mosesvk <kaumatule93@gmail.com>
2021-12-26 23:35:51 -0700

    update

--- 01-starting-setup/src/places/pages/NewPlace.js
+++ 01-starting-setup/src/places/pages/NewPlace.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import Input from '../../shared/components/FormElements/Input'
-import './NewPlace.css'
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
 
 const NewPlace = () => {
   return (
-    <form className='place-form'>
-      <Input element='input' type='text' label='Title' />
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter a valid title"
+      />
     </form>
-  )
-}
+  );
+};
 
-export default NewPlace
+export default NewPlace;
