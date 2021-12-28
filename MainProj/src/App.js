0be21794acca37cd06a2ecb68de832f52133b441
mosesvk <kaumatule93@gmail.com>
2021-12-27 21:08:28 -0700

    update

--- MainProj/src/App.js
+++ MainProj/src/App.js
@@ -11,7 +11,6 @@ import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
-import UpdatePlace from './places/pages/UpdatePlace';
 
 const App = () => {
   return (
@@ -22,7 +21,7 @@ const App = () => {
           <Route path="/" exact component={Users} />
           <Route path="/:userId/places" exact component={UserPlaces} />
           <Route path="/places/new" exact component={NewPlace} />
-          <Route path="/places/:placeId" exact component={UpdatePlace}/>
+          <Route path="/places/:placeId" component={UpdatePlace}/>
           <Redirect to="/" />
         </Switch>
       </main>
