3a2a3b22ef100be37e7ce6ced35f16105e52d4e2
mosesvk <kaumatule93@gmail.com>
2021-12-29 20:47:33 -0700

    update

--- MainProj/src/places/pages/UserPlaces.js
+++ MainProj/src/places/pages/UserPlaces.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
-import Button from '../../shared/components/FormElements/Button';
 
 const DUMMY_PLACES = [
   {
