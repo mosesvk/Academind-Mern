434a75b92968dbccd2d1369d31c3def106ae12ad
mosesvk <kaumatule93@gmail.com>
2022-01-05 14:56:36 -0700

    update

--- MainProj/src/App.js
+++ MainProj/src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 
 import Users from './user/pages/Users';
+import Auth from './user/pages/Auth';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
@@ -18,18 +19,11 @@ const App = () => {
       <MainNavigation />
       <main>
         <Switch>
-          <Route path="/" exact>
-            <Users />
-          </Route>
-          <Route path="/:userId/places" exact>
-            <UserPlaces />
-          </Route>
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
-          <Route path="/places/:placeId">
-            <UpdatePlace />
-          </Route>
+          <Route path="/" exact component={Users}/>
+          <Route path='/auth' exact component={Auth}/>
+          <Route path="/:userId/places" exact component={UserPlaces} />
+          <Route path="/places/new" exact component={NewPlace}/>
+          <Route path="/places/:placeId" component={UpdatePlace}/>
           <Redirect to="/" />
         </Switch>
       </main>
