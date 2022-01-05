df5c814d983d77168895508f3655c4f71d9eac1f
mosesvk <kaumatule93@gmail.com>
2022-01-05 15:44:47 -0700

    update

--- MainProj/src/user/pages/Auth.js
+++ MainProj/src/user/pages/Auth.js
@@ -1,13 +1,31 @@
 import React from 'react'
 
 import Card from '../../shared/components/UIElements/Card'
+import Input from '../../shared/components/FormElements/Input'
+import Button from '../../shared/components/FormElements/Button'
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from '../../shared/util/validators'
+import { useForm } from '../../shared/hooks/form-hook'
 import './Auth.css'
 
 const Auth = () => {
+  const [formState, inputHandler] = useForm({
+    email: {
+      value: '',
+      isValid: false
+    },
+    password: {
+      value: '',
+      isValid: false
+    }
+  })
+
   return (
     <Card className='authentication'>
       <h2>Login Required</h2>
-      <form></form>
+      <hr/>
+      <form>
+        <Input element='input' id='email' type='email' label='E-mail' validators={[VALIDATOR_EMAIL()]} errorText='Please enter a valid email address' onChange={inputHandler} />
+      </form>
     </Card>
   )
 }
