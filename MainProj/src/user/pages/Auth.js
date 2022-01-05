9d30be16408155ae842f6493e4e15946beadec18
mosesvk <kaumatule93@gmail.com>
2022-01-05 15:47:14 -0700

    update

--- MainProj/src/user/pages/Auth.js
+++ MainProj/src/user/pages/Auth.js
@@ -1,33 +1,54 @@
-import React from 'react'
+import React from "react";
 
-import Card from '../../shared/components/UIElements/Card'
-import Input from '../../shared/components/FormElements/Input'
-import Button from '../../shared/components/FormElements/Button'
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from '../../shared/util/validators'
-import { useForm } from '../../shared/hooks/form-hook'
-import './Auth.css'
+import Card from "../../shared/components/UIElements/Card";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+import "./Auth.css";
 
 const Auth = () => {
   const [formState, inputHandler] = useForm({
     email: {
-      value: '',
-      isValid: false
+      value: "",
+      isValid: false,
     },
     password: {
-      value: '',
-      isValid: false
-    }
-  })
+      value: "",
+      isValid: false,
+    },
+  });
 
   return (
-    <Card className='authentication'>
+    <Card className="authentication">
       <h2>Login Required</h2>
-      <hr/>
+      <hr />
       <form>
-        <Input element='input' id='email' type='email' label='E-mail' validators={[VALIDATOR_EMAIL()]} errorText='Please enter a valid email address' onChange={inputHandler} />
+        <Input
+          element="input"
+          id="email"
+          type="email"
+          label="E-mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address"
+          onChange={inputHandler}
+        />
+        <Input
+          element="input"
+          id="password"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid password, at least 5 characters"
+          onChange={inputHandler}
+        />
+        <Button type='submit'>LOGIN</Button>
       </form>
     </Card>
-  )
-}
+  );
+};
 
-export default Auth
+export default Auth;
