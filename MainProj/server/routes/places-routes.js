dba39a29f201950148ea14bba81e971c960ecc22
mosesvk <kaumatule93@gmail.com>
2022-01-06 07:58:11 -0700

    update

--- MainProj/server/routes/places-routes.js
+++ MainProj/server/routes/places-routes.js
@@ -4,20 +4,23 @@ const router = express.Router();
 
 const DUMMY_PLACES = [
   {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
     location: {
-      lat:40.7484474, 
-      lng:-73.9871516
+      lat: 40.7484474,
+      lng: -73.9871516,
     },
-    address: '20 W 34th St, New York, NY 10001',
-    u1: 'u1'
-  }
-]
+    address: "20 W 34th St, New York, NY 10001",
+    u1: "u1",
+  },
+];
 
-router.get("/", (req, res, next) => {
-  console.log("GET Request in Places");
+router.get("/pid", (req, res, next) => {
+  const placeId = req.params.pid // params -> { pid: 'p1' }
+  const place = DUMMY_PLACES.find(item => {
+    return item.id === placeId
+  })
   res.json({ message: "It Works!" });
 });
 
