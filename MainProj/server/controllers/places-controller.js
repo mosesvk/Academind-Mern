716a02c6fc10c564eaec45900ca9580bb659d72b
mosesvk <kaumatule93@gmail.com>
2022-01-07 12:30:15 -0700

    update

--- MainProj/server/controllers/places-controller.js
+++ MainProj/server/controllers/places-controller.js
@@ -3,6 +3,7 @@ const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require('express-validator')
 
 const HttpError = require("../models/error-http");
+const getCoordsForAddress = require('../util/location')
 
 let DUMMY_PLACES = [
   {
@@ -50,15 +51,23 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async  (req, res, next) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     console.log(errors)
     return next(new HttpError('Invalid inputs passed, please check your data', 422))
   }
-
   // taken from the bodyParser in main app.js
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  // using google api to extract coordinates address
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address)
+  } catch(error) {
+    return next(error)
+  }
+  
 
   const createdPlace = {
     id: uuidv4(),
--- MainProj/server/util/location.js
+++ MainProj/server/util/location.js
@@ -1,15 +1,28 @@
-const HttpError = require('../models/error-http')
+const HttpError = require('../models/error-http');
+const axios = require('axios')
 
 const API_KEY = 'AIzaSyCWvdYUvMMFnRAaevMBQcp20oLW_0uVKXM';
 
-const getCoordinatesForAddress = async (address) => {
+const getCoordsForAddress = async (address) => {
   const response = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/address=${encoodeURIComponent(address)}&key=${API_KEY}`
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY} `
   );
 
-  const data = response.data
+  const data = response.data;
 
   if (!data || data.status === 'ZERO_RESULTS') {
-    const error = new HttpError('Could not find location for the specified address', 422)
+    const error = new HttpError(
+      'Could not find location for the specified address',
+      422
+    );
+    return next(error)
   }
+
+  const coordinates = data.results[0].geometry.location
+
+  return coordinates
 };
+
+module.exports = getCoordsForAddress
