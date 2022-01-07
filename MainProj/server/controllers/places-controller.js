f1558b4beddfa2bef7189152460904b387aaa7c5
mosesvk <kaumatule93@gmail.com>
2022-01-06 17:16:25 -0700

    update

--- MainProj/server/controllers/places-controller.js
+++ MainProj/server/controllers/places-controller.js
@@ -31,21 +31,21 @@ const getPlaceById = (req, res, next) => {
   res.json({ place }); // { place } = { place: place }
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
-  const place = DUMMY_PLACES.find((item) => {
+  const places = DUMMY_PLACES.filter((item) => {
     return item.creator === userId;
   });
 
-  if (!place) {
+  if (!places || places.length === 0) {
     // from models folder
     return next(
-      new HttpError("Could not find place for the provided user id", 404)
+      new HttpError("Could not find places for the provided user id", 404)
     );
   }
 
-  res.json({ place });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -90,7 +90,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
