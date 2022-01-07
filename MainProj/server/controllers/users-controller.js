8e5456e4015114385012808b8698944e9c8d764c
mosesvk <kaumatule93@gmail.com>
2022-01-06 21:43:14 -0700

    update

---
+++ MainProj/server/controllers/users-controller.js
@@ -0,0 +1,47 @@
+const { v4: uuidv4 } = require("uuid");
+
+const HttpError = require('../models/error-http')
+
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "Moses Kaumatule",
+    username: 'test',
+    email: "test@test.com",
+    password: "testers",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.status(200).json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const {name, username, email, password} = req.body;
+
+  const createdUser = {
+    id: uuidv4(),
+    name, 
+    username,
+    email, 
+    password
+  }
+
+  DUMMY_USERS.push(createdUser)
+
+  res.status(201).json({message: 'Successfully added user', user: createdUser})
+};
+
+const login = (req, res, next) => {
+  const {email, password} = req.body;
+
+  const identifiedUser = DUMMY_USERS.find(user => user.email === email)
+  if (!identifiedUser || identifiedUser.password !== password) {
+    next(new HttpError('Could not identify user. Credentials seem to be wrong', 401))
+  }
+
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
