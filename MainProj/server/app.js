64d33f09fde8080739cd0a866403762f9e942014
mosesvk <kaumatule93@gmail.com>
2022-01-07 21:48:49 -0700

    update

--- MainProj/server/app.js
+++ MainProj/server/app.js
@@ -1,22 +1,26 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require("./routes/users-routes");
-const HttpError = require("./models/error-http");
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/error-http');
+
+const url =
+  'mongodb+srv://mosesvk:Lukifanga2656@cluster0.4gc6f.mongodb.net/places?retryWrites=true&w=majority';
 
 const app = express();
 
 app.use(bodyParser.json());
 
 // MIDDLEWARE
-app.use("/api/places", placesRoutes); // '/api/places'
+app.use('/api/places', placesRoutes); // '/api/places'
 
-app.use("/api/users", usersRoutes);
+app.use('/api/users', usersRoutes);
 
 // handles errors for unsupported routes
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route", 404);
+  const error = new HttpError('Could not find this route', 404);
   next(error);
 });
 
@@ -26,7 +30,12 @@ app.use((error, req, res, next) => {
   }
 
   res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occured!" });
+  res.json({ message: error.message || 'An unknown error occured!' });
 });
 
-app.listen(5555);
+mongoose
+  .connect(url)
+  .then(() => {
+    app.listen(5555);
+  })
+  .catch((err) => console.log(err));
