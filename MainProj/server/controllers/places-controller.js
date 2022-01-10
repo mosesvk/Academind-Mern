6bd728236714103c02accb31823a8c810f4dd4a9
mosesvk <kaumatule93@gmail.com>
2022-01-09 21:44:23 -0700

    update

--- MainProj/server/controllers/places-controller.js
+++ MainProj/server/controllers/places-controller.js
@@ -51,27 +51,29 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
+  // let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate('places');
   } catch (err) {
     const error = new HttpError(
-      'Fetching places failed, please try again',
+      'Fetching places failed, please try again later',
       500
     );
-
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  // if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
-      new HttpError('Could not find any places for the provided userId', 404)
+      new HttpError('Could not find places for the provided user id.', 404)
     );
   }
 
-  // we cannot use the place.toObject() like we did in getPlaceById() line 46... because the .find() method on line 54 returns an object and error
   res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.places.map((place) =>
+      place.toObject({ getters: true })
+    ),
   });
 };
 
