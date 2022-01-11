730daa5e10de90cdc15df6cd8be4aef7841b4e1d
mosesvk <kaumatule93@gmail.com>
2022-01-11 14:09:01 -0700

    update

--- MainProj/frontend/src/shared/components/FormElements/ImageUpload.js
+++ MainProj/frontend/src/shared/components/FormElements/ImageUpload.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
+import Button from './Button'
 import './ImageUpload.css';
 
 const ImageUpload = (props) => {
+  const filePickerRef = useRef()
+
+  const pickHandler = (event) => {
+    console.log(event.target)
+  }
+
+  const pickImageHandler = () => {
+    filePickerRef.current.click();
+
+  }
+
   return (
     <div className='form-control'>
       <input
         id={props.id}
+        ref={filePickerRef}
         style={{ display: 'none' }}
         type='file'
-        accept='.jpg, .png, .jpeg'
+        accept='.jpg,.png,.jpeg'
+        onChange={pickHandler}
       />
+      <div className={`image-upload ${props.center && 'center'}`}>
+        <div className='image-upload__preview'>
+          <img src='' alt='preview'/>
+        </div>
+        <Button type='button' onClick={pickImageHandler}>Pick Image</Button>
+      </div>
     </div>
   );
 };
