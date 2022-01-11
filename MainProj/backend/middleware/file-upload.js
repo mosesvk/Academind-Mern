899856ce16134a125a6855684d328ea3f265e91f
mosesvk <kaumatule93@gmail.com>
2022-01-11 15:28:40 -0700

    update

--- MainProj/backend/middleware/file-upload.js
+++ MainProj/backend/middleware/file-upload.js
@@ -10,11 +10,14 @@ const MIME_TYPE_MAP = {
 const fileUpload = multer({
   limits: 500000,
   storage: multer.diskStorage({
-    destination: (req, file, cb) => {},
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/images')
+    },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype]
-      cb(null, uuid())
-    }
+      cb(null, uuid() + '.' + ext)
+    },
+    fileFilter: 
   })
 })
 
