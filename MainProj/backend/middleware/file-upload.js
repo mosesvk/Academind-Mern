53382ad65921d5744ee9f7ecf2f76822db4aacb5
mosesvk <kaumatule93@gmail.com>
2022-01-11 15:22:34 -0700

    update

--- MainProj/backend/middleware/file-upload.js
+++ MainProj/backend/middleware/file-upload.js
@@ -1,7 +1,11 @@
 const multer = require('multer')
 
 const fileUpload = multer({
-  
+  limits: 500000,
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {},
+    filename: (req, file, cb) => {}
+  })
 })
 
 module.exports = fileUpload
