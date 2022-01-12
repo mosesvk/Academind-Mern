380e01cfbac09b8c48bd39b5f3e39ebb6ab88104
mosesvk <kaumatule93@gmail.com>
2022-01-12 15:17:25 -0700

    update

--- MainProj/backend/middleware/check-auth.js
+++ MainProj/backend/middleware/check-auth.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+
 const HttpError = require('../models/http-error');
 
 module.exports = (req, res, next) => {
@@ -8,6 +10,10 @@ module.exports = (req, res, next) => {
     if (!token) {
       return next(new HttpError('authorization.split undefined. check req.'))
     }
+    const decodedToken = jwt.verify(token, 'supersecret_dont_share')
+    req.userData = { userId: decodedToken.userId }
+    next();
+
   } catch (err) {
     const error = new HttpError('Authorization failed!', 401);
     return next(error);
