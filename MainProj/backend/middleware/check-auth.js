6824e08a60318cdb4de359bedfbb5398ae0e179a
mosesvk <kaumatule93@gmail.com>
2022-01-12 15:05:53 -0700

    update

---
+++ MainProj/backend/middleware/check-auth.js
@@ -0,0 +1,15 @@
+const HttpError = require('../models/http-error');
+
+module.exports = (req, res, next) => {
+  // Authorization: 'Bearer TOKEN' so we can access that TOKEN by going into authorization[1]
+  let token;
+  try {
+    token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+      return next(new HttpError('authorization.split undefined. check req.'))
+    }
+  } catch (err) {
+    const error = new HttpError('Authorization failed!', 401);
+    return next(error);
+  }
+};
