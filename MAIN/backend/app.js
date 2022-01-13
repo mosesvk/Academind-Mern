7b202e8b90462387ab0abaf82023efd773ba6722
mosesvk <kaumatule93@gmail.com>
2022-01-12 21:06:57 -0700

    update

--- MAIN/backend/app.js
+++ MAIN/backend/app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://mosesveuki:solia@cluster0.4gc6f.mongodb.net/places?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4gc6f.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
