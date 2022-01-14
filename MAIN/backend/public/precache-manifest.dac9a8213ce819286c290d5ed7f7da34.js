36532648510a9e9fa8e079a0f3112d82eb0ce592
mosesvk <kaumatule93@gmail.com>
2022-01-14 10:02:10 -0700

    added new react frontend version

---
+++ MAIN/backend/public/precache-manifest.dac9a8213ce819286c290d5ed7f7da34.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ab3687b5bf43b8e5129827ba9aea8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebc26c792601d0902c72",
+    "url": "/static/css/0.9d512f0e.chunk.css"
+  },
+  {
+    "revision": "81280aa73b384e91063a",
+    "url": "/static/css/4.6d344725.chunk.css"
+  },
+  {
+    "revision": "8991eb1e52dcc5994a19",
+    "url": "/static/css/5.b957ddbe.chunk.css"
+  },
+  {
+    "revision": "65b71154b85324184b2a",
+    "url": "/static/css/6.a60aebec.chunk.css"
+  },
+  {
+    "revision": "d95cbc038e14151ca1e2",
+    "url": "/static/css/7.d37dee14.chunk.css"
+  },
+  {
+    "revision": "0949119c077cd5f19f44",
+    "url": "/static/css/8.12c34369.chunk.css"
+  },
+  {
+    "revision": "4f6181952c273c337678",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "ebc26c792601d0902c72",
+    "url": "/static/js/0.fead626d.chunk.js"
+  },
+  {
+    "revision": "2a6b15382c099562dd5c",
+    "url": "/static/js/3.0d52b6e6.chunk.js"
+  },
+  {
+    "revision": "81280aa73b384e91063a",
+    "url": "/static/js/4.b734ed02.chunk.js"
+  },
+  {
+    "revision": "8991eb1e52dcc5994a19",
+    "url": "/static/js/5.829fba6e.chunk.js"
+  },
+  {
+    "revision": "65b71154b85324184b2a",
+    "url": "/static/js/6.c03a47c5.chunk.js"
+  },
+  {
+    "revision": "d95cbc038e14151ca1e2",
+    "url": "/static/js/7.7a163c85.chunk.js"
+  },
+  {
+    "revision": "0949119c077cd5f19f44",
+    "url": "/static/js/8.061fecdb.chunk.js"
+  },
+  {
+    "revision": "4f6181952c273c337678",
+    "url": "/static/js/main.c0b4c34b.chunk.js"
+  },
+  {
+    "revision": "d1c79971f6c09b42d81d",
+    "url": "/static/js/runtime-main.656dc0c8.js"
+  }
+]);
